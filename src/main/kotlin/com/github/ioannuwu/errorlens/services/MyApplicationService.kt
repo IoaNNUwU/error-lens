d33f0122518de2f52d94d0f4af09e29623ad9ea1
GitHub Action <action@github.com>
2022-12-04 20:59:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/ioannuwu/errorlens/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ioannuwu/errorlens/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ioannuwu.errorlens.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ioannuwu.errorlens.MyBundle
 
 class MyApplicationService {
 
