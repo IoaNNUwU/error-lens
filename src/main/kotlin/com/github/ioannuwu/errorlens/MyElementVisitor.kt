c306a7c611f52ce82f15cd70312c182267346815
IoaNNUwU <ioannxd@gmail.com>
2022-12-17 20:21:03 +0200

    Highligters work without crashes

--- src/main/kotlin/com/github/ioannuwu/errorlens/MyElementVisitor.kt
+++ src/main/kotlin/com/github/ioannuwu/errorlens/MyElementVisitor.kt
@@ -1,25 +1,14 @@
 package com.github.ioannuwu.errorlens
 
-import com.github.ioannuwu.errorlens.utils.HintData
-import com.github.ioannuwu.errorlens.utils.MyTextAttributes
-import com.intellij.codeInsight.daemon.impl.HighlightInfo
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.editor.EditorCustomElementRenderer
-import com.intellij.openapi.editor.Inlay
 import com.intellij.openapi.editor.colors.EditorFontType
 import com.intellij.openapi.editor.impl.DocumentMarkupModel
-import com.intellij.openapi.editor.markup.GutterIconRenderer
-import com.intellij.openapi.editor.markup.TextAttributes
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiFile
 import com.intellij.refactoring.suggested.endOffset
 import com.jetbrains.rd.util.printlnError
 import java.awt.Color
-import java.awt.Graphics
-import java.awt.Rectangle
-import javax.swing.Icon
 
 class MyElementVisitor(
     private val fileEditorManager: FileEditorManager,
@@ -28,49 +17,40 @@ class MyElementVisitor(
     override fun visitFile(file: PsiFile) {
 
         ApplicationManager.getApplication().invokeLater {
-
-            val highlighters = DocumentMarkupModel
-                    .forDocument(file.viewProvider.document, file.project, false)
-                    .allHighlighters
-
             val selectedEditor = fileEditorManager.selectedTextEditor ?: return@invokeLater
 
             selectedEditor.inlayModel.getAfterLineEndElementsInRange(0, file.endOffset).forEach { it.dispose() }
-            selectedEditor.markupModel.allHighlighters.forEach { it.dispose() }
 
-
-
-            val collection: MutableSet<HighlightInfo> = mutableSetOf()
-
-            highlighters.asSequence()
+            DocumentMarkupModel.forDocument(file.viewProvider.document, file.project, false).allHighlighters.asSequence()
                     .filterNotNull()
-                    .filter { it.errorStripeTooltip is HighlightInfo }
-                    .map { it.errorStripeTooltip as HighlightInfo }
-                    .filter { it.severity.toString() != SYMBOL_TYPE_SEVERITY }
-                    .filter { it.description != null }
-                    .toCollection(collection)
+                    .forEach { it.setCustomRenderer { editor, highlighter, g ->
+                        g.font = editor.colorsScheme.getFont(EditorFontType.PLAIN)
+                        g.color = Color.RED
 
-            for (info in collection) {
+                        val document = editor.document
+                        val lineNumber = document.getLineNumber(highlighter.startOffset)
+                        val lineEndOffset = document.getLineEndOffset(lineNumber)
 
-                printlnError(info.toString())
+                        val position = editor.offsetToXY(lineEndOffset)
 
-                val startOffset = info.startOffset
-                val description = info.description!!
-                val line = selectedEditor.document.getLineNumber(startOffset)
+                        g.drawString(
+                                highlighter.errorStripeTooltip.toString(),
+                                position.x + CLOWN,
+                                position.y + editor.ascent,
+                        )
+                    } }
 
-                val hint = HintData(line, description)
-
-                if (selectedEditor.inlayModel.getAfterLineEndElementsForLogicalLine(line).isEmpty()) {
-                    selectedEditor.inlayModel.addAfterLineEndElement(startOffset, false, hint)
-                }
-                selectedEditor.markupModel.addLineHighlighter(line, 0, MyTextAttributes(hint))
-
+            selectedEditor.markupModel.allHighlighters.forEach {
+                printlnError("""
+                    real: $it
+                """.trimIndent())
             }
-            printlnError("====")
         }
+
     }
 }
 
+private const val CLOWN = 15
 private const val SYMBOL_TYPE_SEVERITY = "SYMBOL_TYPE_SEVERITY"
 
 // TODO priority system
