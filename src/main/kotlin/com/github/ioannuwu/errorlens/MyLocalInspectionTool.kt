612036ab88cbf26f987fd2f5b5d196b273e34aea
IoaNNUwU <ioannxd@gmail.com>
2022-12-25 03:17:12 +0200

    hotfix

--- src/main/kotlin/com/github/ioannuwu/errorlens/MyLocalInspectionTool.kt
+++ src/main/kotlin/com/github/ioannuwu/errorlens/MyLocalInspectionTool.kt
@@ -1,7 +1,7 @@
 package com.github.ioannuwu.errorlens
 
-import com.github.ioannuwu.errorlens.errorRender.OnePerLineWithHighestPriority
-import com.github.ioannuwu.errorlens.errorRender.renderData.ErrorRenderDataSelectorImpl
+import com.github.ioannuwu.errorlens.errorRender.HighlightersSelector
+import com.github.ioannuwu.errorlens.errorRender.renderData.ErrorRenderDataSelector
 import com.github.ioannuwu.errorlens.gui.MySettingsService
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
@@ -10,8 +10,8 @@ import com.intellij.openapi.application.ApplicationManager
 class MyLocalInspectionTool : LocalInspectionTool() {
 
     private val settingsService = ApplicationManager.getApplication().getService(MySettingsService::class.java)
-    private val highlightersSelector = OnePerLineWithHighestPriority()
-    private val errorDataSelector = ErrorRenderDataSelectorImpl(settingsService)
+    private val highlightersSelector = HighlightersSelector.OnePerLineWithHighestPriority()
+    private val errorDataSelector = ErrorRenderDataSelector.DataSelectorFromSettingsService(settingsService)
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) =
             MyElementVisitor(settingsService.numberWhitespaces, highlightersSelector, errorDataSelector)
